414b153801b21d680b5e2ecb94d027cebedc0150
Ben Schwartz <benschw@gmail.com>
2015-06-19 14:06:38 +0000

    making aggregate provider work recursively

--- Fliglio/Config/AggregatePropertySetProvider.php
+++ Fliglio/Config/AggregatePropertySetProvider.php
@@ -22,13 +22,30 @@ class AggregatePropertySetProvider implements PropertySetProvider {
 	private function generatePropertySet() {
 		$config = [];
 		foreach ($this->sources as $source) {
-			$config = $this->applyPropertySetProviders($config, $source);
+			$config = $this->overlayProperties($config, $source->build());
 		}
 		return $config;
 	}
-	private function applyPropertySetProviders(array $config, $source) {
-		return array_merge($config, $source->build());
-	 }
+	/**
+	 * Recursively merge two arrays
+	 * - scalar values with the same key:  overlay's value is used
+	 * - assoc arrays with the same key:   merge two associative arrays
+	 * - ordered arrays with the same key: overlay's array is used
+	 */
+	private function overlayProperties($base, $overlay) {
+		foreach ($overlay as $key => $val) {
+			if (is_array($val) && $this->isAssociative($val) && isset($base[$key])) {
+				$base[$key] = $this->overlayProperties($base[$key], $val);
+			} else {
+				$base[$key] = $val;
+			}
+		}
+		return $base;
+	}
 
+	// test that keys aren't sequential numeric values starting at 0
+	private function isAssociative(array $arr) {
+		return array_keys($arr) !== range(0, count($arr) - 1);
+	}
 }
 
--- test/PropertySetTest.php
+++ test/PropertySetTest.php
@@ -18,11 +18,64 @@ class PropertySetTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($cfg, $found);
 	}
 
-	public function testAggregateProvider() {
+	public function testAggregateProvider_ShallowConfigs() {
 		$cfgA = new DefaultPropertySetProvider(["foo" => "bar", "baz" => "biz"]);
 		$cfgB = new DefaultPropertySetProvider(["b" => "BBBB", "foo" => "updated"]);;
 		
 		$expected = ["b" => "BBBB", "foo" => "updated", "baz" => "biz"];
+
+		// when
+		$p = new AggregatePropertySetProvider([$cfgA, $cfgB]);
+		$found = $p->build();
+
+		// then
+		$this->assertEquals($found, $expected);
+	}
+	public function testAggregateProvider_DeepConfigs() {
+		$cfgA = new DefaultPropertySetProvider([
+			"a"   => "afoo",
+			"a2"  => "afoo",
+			"arr" => ["a1", "a2", "a3"],
+			"obj" => [
+				"a" => "afoo",
+				"arr" => ["a1", "a2", "a3"],
+				"obj" => [
+					"a"  => "afoo",
+					"a2" => "afoo",
+				]
+			],
+		]);
+		$cfgB = new DefaultPropertySetProvider([
+			"a"   => "bfoo",
+			"b2"  => "bfoo",
+			"arr" => ["b1", "b2"],
+			"obj" => [
+				"a" => "bfoo",
+				"arr" => ["b1", "b2"],
+				"obj" => [
+					"arr" => ["a1", "a2", "a3"],
+					"a"  => "bfoo",
+					"b2" => "bfoo",
+				]
+			],
+		]);
+		
+		$expected = [
+			"a"   => "bfoo",
+			"a2"  => "afoo",
+			"b2"  => "bfoo",
+			"arr" => ["b1", "b2"],
+			"obj" => [
+				"a" => "bfoo",
+				"arr" => ["b1", "b2"],
+				"obj" => [
+					"arr" => ["a1", "a2", "a3"],
+					"a"  => "bfoo",
+					"a2" => "afoo",
+					"b2" => "bfoo",
+				]
+			],
+		];
 
 		// when
 		$p = new AggregatePropertySetProvider([$cfgA, $cfgB]);
